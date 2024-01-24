a75d71b60f2432ea627fe66644ad751e282bf698
BeeTillman <bpt3creations@gmail.com>
2024-01-24 00:44:17 -0500

    Removed Unnecessary Files removed: fun.py, template.py removed: 8ball command -> general.py removed: UPDATES.md

--- cogs/general.py
+++ cogs/general.py
@@ -206,48 +206,6 @@ class General(commands.Cog, name="general"):
             await context.send(embed=embed)
 
     @commands.hybrid_command(
-        name="8ball",
-        description="Ask any question to the bot.",
-    )
-    @app_commands.describe(question="The question you want to ask.")
-    async def eight_ball(self, context: Context, *, question: str) -> None:
-        """
-        Ask any question to the bot.
-
-        :param context: The hybrid command context.
-        :param question: The question that should be asked by the user.
-        """
-        answers = [
-            "It is certain.",
-            "It is decidedly so.",
-            "You may rely on it.",
-            "Without a doubt.",
-            "Yes - definitely.",
-            "As I see, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again later.",
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful.",
-        ]
-        embed = discord.Embed(
-            title="**My Answer:**",
-            description=f"{random.choice(answers)}",
-            color=0xBEBEFE,
-        )
-        embed.set_footer(text=f"The question was: {question}")
-        await context.send(embed=embed)
-
-    @commands.hybrid_command(
         name="bitcoin",
         description="Get the current price of bitcoin.",
     )
